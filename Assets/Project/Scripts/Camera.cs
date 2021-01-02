903292bc32b34d20267e108e6a5b8cef98d6b390
tomer <tomerkubovsky@gmail.com>
2021-01-02 17:54:36 +1100

    Initial Comit
    This is our first comit

---
+++ Assets/Project/Scripts/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false ;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Cursor.visible = true)
+            Cursor.visible = false ;
+            Cursor.lockState = CursorLockMode.Confined;
+        transform.Rotate(Input.GetAxis("Mouse Y")*-1*2,0f,0f);
+    }
+}
---
+++ Assets/Project/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed = 10.0f;
+    public Rigidbody rb;
+    public Vector3 Direction;
+    public float thrust = 3;
+    public bool IsJumping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //movement
+        Walk();
+        //jump
+        if (Input.GetKeyDown("space") == true && IsJumping == false)
+        {
+            rb.AddForce(Vector3.up * thrust * 100);
+            IsJumping = true;
+        }
+        if (rb.velocity == new Vector3 (0f,0f,0f))
+        {
+            Debug.Log(IsJumping);
+            IsJumping = false;
+            Debug.Log(IsJumping);
+        }
+        //rotatetion here
+        transform.Rotate(0f,Input.GetAxis("Mouse X")*2,0f);
+    }
+    void FixedUpdate()
+    {
+    }
+    void Walk()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.position += transform.forward * Speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position += transform.forward *-1 * Speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position += transform.right *-1 * Speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += transform.right * Speed * Time.deltaTime;
+        }
+    }
+}
